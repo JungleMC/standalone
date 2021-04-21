3057bf1ad253f06947dc51790952626b0925968b
Ella Fox <ella@fox.gal>
2021-04-21 20:01:15 +0100

    Debug logging; block data and loading

--- cmd/JungleTree.go
+++ cmd/JungleTree.go
@@ -3,13 +3,41 @@ package main
 import (
     "github.com/junglemc/JungleTree/internal/handlers"
     "github.com/junglemc/net"
+    "github.com/junglemc/world/blocks"
     "log"
+    "path/filepath"
+)
+
+const (
+    JungleTreeVersion = "0.0.3" // TODO: Load from git or tags?
+    MinecraftVersion  = "1.16.5"
+    thinLine          = "------------------------------------"
+    thickLine         = "===================================="
 )
 
 func main() {
     s := net.NewServer("0.0.0.0", 25565, true, 256, true, handlers.Handshake, handlers.Status, handlers.Login, nil)
 
-    err := s.Listen()
+    log.Println(thickLine)
+    log.Println("Starting JungleTree Server v" + JungleTreeVersion)
+    log.Println(thickLine)
+
+    if s.Debug {
+        log.Println("\t* Loading blocks")
+    }
+
+    err := blocks.LoadBlocks(filepath.Join("configs", MinecraftVersion, "blocks.json"))
+    if err != nil {
+        log.Panicln(err)
+    }
+
+    if s.Debug {
+        log.Println("Done!")
+        log.Println(thinLine)
+    }
+
+    log.Printf("Server listening on: %s:%d", s.Address, s.Port)
+    err = s.Listen()
     if err != nil {
         log.Panicln(err)
     }
--- go.mod
+++ go.mod
@@ -5,10 +5,15 @@ go 1.16
 require (
 	github.com/google/uuid v1.2.0
 	github.com/junglemc/mc v0.0.0-20210331135753-2c933d019f59
-	github.com/junglemc/nbt v1.0.0
+	github.com/junglemc/nbt v1.1.2
 	github.com/junglemc/net v0.0.0-20210331152110-1745234921ff
+	github.com/junglemc/world v0.0.0-20210421105504-3d6560b6a2d0
 )
 
 replace github.com/junglemc/mc => /home/ella/go/src/github.com/junglemc/mc
+
 replace github.com/junglemc/nbt => /home/ella/go/src/github.com/junglemc/nbt
+
 replace github.com/junglemc/net => /home/ella/go/src/github.com/junglemc/net
+
+replace github.com/junglemc/world => /home/ella/go/src/github.com/junglemc/world
