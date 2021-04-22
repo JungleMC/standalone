830797b5b2f37b5d9aeb27399344cf6770012095
Ella Fox <ella@fox.gal>
2021-04-22 12:43:37 +0100

    Load biomes, use embedded file storage with Go 1.16 ^.^

--- cmd/JungleTree.go
+++ cmd/JungleTree.go
@@ -1,44 +1,60 @@
 package main
 
 import (
-    "github.com/junglemc/JungleTree/internal/handlers"
-    "github.com/junglemc/net"
-    "github.com/junglemc/world"
-    "log"
-    "path/filepath"
+	_ "embed"
+	"github.com/junglemc/JungleTree/internal/handlers"
+	"github.com/junglemc/net"
+	"github.com/junglemc/world"
+	"log"
 )
 
 const (
-    JungleTreeVersion = "0.0.3" // TODO: Load from git or tags?
-    MinecraftVersion  = "1.16.5"
-    thinLine          = "------------------------------------"
-    thickLine         = "===================================="
+	JungleTreeVersion = "0.0.3" // TODO: Load from git or tags?
+	MinecraftVersion  = "1.16.5"
+	thinLine          = "------------------------------------"
+	thickLine         = "===================================="
 )
 
 func main() {
-    s := net.NewServer("0.0.0.0", 25565, true, 256, true, handlers.Handshake, handlers.Status, handlers.Login, nil)
+	s := net.NewServer("0.0.0.0", 25565, true, 256, true, handlers.Handshake, handlers.Status, handlers.Login, nil)
 
-    log.Println(thickLine)
-    log.Println("Starting JungleTree Server v" + JungleTreeVersion)
-    log.Println(thickLine)
+	log.Println(thickLine)
+	log.Println("Starting JungleTree Server v" + JungleTreeVersion)
+	log.Println(thickLine)
 
-    if s.Debug {
-        log.Println("\t* Loading blocks")
-    }
+	loadBlocks(s)
+	loadBiomes(s)
 
-    err := world.LoadBlocks(filepath.Join("configs", MinecraftVersion, "blocks.json"))
-    if err != nil {
-        log.Panicln(err)
-    }
+	if s.Debug {
+		log.Println("Done!")
+		log.Println(thinLine)
+	}
 
-    if s.Debug {
-        log.Println("Done!")
-        log.Println(thinLine)
-    }
+	log.Printf("Server listening on: %s:%d", s.Address, s.Port)
+	err := s.Listen()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
 
-    log.Printf("Server listening on: %s:%d", s.Address, s.Port)
-    err = s.Listen()
-    if err != nil {
-        log.Panicln(err)
-    }
+func loadBlocks(s *net.Server) {
+	if s.Debug {
+		log.Println("\t* Loading blocks")
+	}
+
+	err := world.LoadBlocks()
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func loadBiomes(s *net.Server) {
+	if s.Debug {
+		log.Println("\t* Loading biomes")
+	}
+
+	err := world.LoadBiomes()
+	if err != nil {
+		log.Panicln(err)
+	}
 }
