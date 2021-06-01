e83bca0ea463d81ef5b01be0009dc6070413005d
Ella Fox <ella@fox.gal>
2021-06-01 16:27:07 +0100

    Bump version

--- cmd/JungleTree.go
+++ cmd/JungleTree.go
@@ -15,7 +15,7 @@ import (
 )
 
 const (
-	JungleTreeVersion = "0.0.7" // TODO: Load from git or tags?
+	JungleTreeVersion = "0.0.8" // TODO: Load from git or tags?
 	thinLine          = "------------------------------------"
 	thickLine         = "===================================="
 	TicksPerSecond    = 20
