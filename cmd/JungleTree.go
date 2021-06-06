b93805b5f87e03d7416e226b0ad19680ddb5bf43
Ella Fox <ella@fox.gal>
2021-06-06 08:12:30 +0100

    Move load functions into startup pkg

--- cmd/JungleTree.go
+++ cmd/JungleTree.go
@@ -1,108 +1,31 @@
 package main
 
 import (
-	"github.com/junglemc/JungleTree/internal/configuration"
-	"github.com/junglemc/JungleTree/internal/net"
-	"github.com/junglemc/JungleTree/internal/net/handlers"
-	"github.com/junglemc/JungleTree/pkg/block"
-	"github.com/junglemc/JungleTree/pkg/crafting"
-	"github.com/junglemc/JungleTree/pkg/entity"
-	"github.com/junglemc/JungleTree/pkg/item"
-	"github.com/junglemc/JungleTree/pkg/world/biome"
-	"github.com/junglemc/JungleTree/pkg/world/dimensions"
-	"log"
-)
-
-const (
-	JungleTreeVersion = "0.0.10" // TODO: Load from git or tags?
-	thinLine          = "------------------------------------"
-	thickLine         = "===================================="
-	TicksPerSecond    = 20
+    "github.com/junglemc/JungleTree/internal/configuration"
+    "github.com/junglemc/JungleTree/internal/net"
+    "github.com/junglemc/JungleTree/internal/net/handlers"
+    "github.com/junglemc/JungleTree/internal/startup"
+    "log"
 )
 
 func main() {
-	conf := configuration.Config()
+    conf := configuration.Config()
 
-	s := net.NewServer(
-		conf.Network.IP, conf.Network.Port, conf.JavaEdition.OnlineMode, conf.Network.NetworkCompressionThreshold,
-		conf.DebugMode, conf.Verbose, handlers.Handshake, handlers.Status, handlers.Login, handlers.Play, net.Disconnect,
-	)
+    s := net.NewServer(
+        conf.Network.IP, conf.Network.Port, conf.JavaEdition.OnlineMode, conf.Network.NetworkCompressionThreshold,
+        conf.DebugMode, conf.Verbose, handlers.Handshake, handlers.Status, handlers.Login, handlers.Play, net.Disconnect,
+    )
 
-	log.Println(thickLine)
-	log.Println("Starting JungleTree Server v" + JungleTreeVersion)
-	log.Println(thickLine)
+    startup.Load()
 
-	loadDimensions()
-	loadBiomes()
-	loadBlocks()
-	loadItems()
-	loadRecipes()
-	loadEntities()
+    addr := s.Address
+    if addr == "" {
+        addr = "*"
+    }
 
-	log.Println("Done!")
-	log.Println(thinLine)
-
-	addr := s.Address
-	if addr == "" {
-		addr = "*"
-	}
-
-	log.Printf("Server listening on: %s:%d", addr, s.Port)
-	err := s.Listen()
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func loadBlocks() {
-	log.Println("\t* Loading blocks")
-
-	err := block.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func loadBiomes() {
-	log.Println("\t* Loading biomes")
-
-	err := biome.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func loadDimensions() {
-	log.Println("\t* Loading dimensions")
-	err := dimensions.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func loadEntities() {
-	log.Println("\t* Loading entities")
-	err := entity.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
-
-	entityThread := entity.Runner{TPS: TicksPerSecond}
-	entityThread.Run()
-}
-
-func loadItems() {
-	log.Println("\t* Loading items")
-	err := item.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
-}
-
-func loadRecipes() {
-	log.Println("\t* Loading recipes")
-	err := crafting.Load()
-	if err != nil {
-		log.Panicln(err)
-	}
+    log.Printf("Server listening on: %s:%d", addr, s.Port)
+    err := s.Listen()
+    if err != nil {
+        log.Panicln(err)
+    }
 }
---
+++ internal/startup/loader.go
@@ -0,0 +1,87 @@
+package startup
+
+import (
+    "github.com/junglemc/JungleTree/pkg"
+    "github.com/junglemc/JungleTree/pkg/block"
+    "github.com/junglemc/JungleTree/pkg/crafting"
+    "github.com/junglemc/JungleTree/pkg/entity"
+    "github.com/junglemc/JungleTree/pkg/item"
+    "github.com/junglemc/JungleTree/pkg/world/biome"
+    "github.com/junglemc/JungleTree/pkg/world/dimensions"
+    "log"
+)
+
+const (
+    thinLine       = "------------------------------------"
+    thickLine      = "===================================="
+    TicksPerSecond = 20
+)
+
+func Load() {
+    log.Println(thickLine)
+    log.Println("Starting JungleTree Server v" + pkg.Version)
+    log.Println(thickLine)
+
+    loadDimensions()
+    loadBiomes()
+    loadBlocks()
+    loadItems()
+    loadRecipes()
+    loadEntities()
+
+    log.Println("Done!")
+    log.Println(thinLine)
+}
+
+func loadBlocks() {
+    log.Println("\t* Loading blocks")
+
+    err := block.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+}
+
+func loadBiomes() {
+    log.Println("\t* Loading biomes")
+
+    err := biome.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+}
+
+func loadDimensions() {
+    log.Println("\t* Loading dimensions")
+    err := dimensions.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+}
+
+func loadEntities() {
+    log.Println("\t* Loading entities")
+    err := entity.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+
+    entityThread := entity.Runner{TPS: TicksPerSecond}
+    entityThread.Run()
+}
+
+func loadItems() {
+    log.Println("\t* Loading items")
+    err := item.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+}
+
+func loadRecipes() {
+    log.Println("\t* Loading recipes")
+    err := crafting.Load()
+    if err != nil {
+        log.Panicln(err)
+    }
+}
